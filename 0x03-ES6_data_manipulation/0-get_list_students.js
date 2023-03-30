fab5ca757e0690c3ced8b4024dea9e4156866d2b
M1urray <rknjonjo@gmail.com>
2023-03-30 10:41:44 +0300

    get list students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+/**
+ * Retrieves a list of students.
+ * @author Robert Kamau <https://github.com/M1urray>
+ */
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+export default getListStudents;
