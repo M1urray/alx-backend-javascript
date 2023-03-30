0d6a5e1d6ca1f17f18ea11b3cc27d05f4cbb9def
M1urray <rknjonjo@gmail.com>
2023-03-30 11:13:33 +0300

    create set from array

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @returns {Set}
+ */
+const setFromArray = array => new Set(array);
+
+export default setFromArray;
