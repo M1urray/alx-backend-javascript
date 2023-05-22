a218ef770896f771d428e7e9de5991daa52ad8e2
M1urray <rknjonjo@gmail.com>
2023-05-22 08:37:48 +0300

    fix string errors

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,13 @@
-const http = require("http");
+const http = require('http');
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const port = 1245;
 const app = http.createServer();
 
-app.on("request", (_, res) => {
-  const responseText = "Hello Holberton School!";
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", responseText.length);
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
